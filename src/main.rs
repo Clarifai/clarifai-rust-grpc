2374c624bf21ffdc05fca109b2ddb2d131f8deea
Rok Povsic <rok.povsic@clarifai.com>
2020-08-04 22:58:11 +0200

    Split main.rs code into functions

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use protobuf::{RepeatedField, ProtobufEnum};
 
 use std::process::exit;
 
+use crate::grpc::status::Status;
 use crate::grpc::status_code::StatusCode;
 use crate::grpc::service_grpc::V2Client;
 use crate::grpc::service::PostModelOutputsRequest;
@@ -21,17 +22,53 @@ mod grpc {
     pub mod status_code;
 }
 
+const GENERAL_MODEL_ID: &str = "aaa03c23b3724a16a56b629203edc62c";
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
 
+    let client = V2Client::new(insecure_grpc());
+
+    let mut req = PostModelOutputsRequest::default();
+    req.set_model_id(GENERAL_MODEL_ID.parse().unwrap());
+    req.set_inputs(make_input_from_file_name(file_name));
+    let response = client
+        .post_model_outputs_opt(&req, call_opt())
+        .expect("Failuree");
+
+    exit_on_failure(response.get_status());
+
+    println!("Predicted concepts:");
+    for concept in response.get_outputs()[0].get_data().get_concepts() {
+        println!("\t{}: {}", concept.get_name(), concept.get_value());
+    }
+}
+
+fn make_input_from_file_name(file_name: &String) -> RepeatedField<Input> {
+    let mut image = Image::new();
+    image.set_base64(read_file(file_name));
+    let mut data = Data::new();
+    data.set_image(image);
+    let mut input = Input::new();
+    input.set_data(data);
+    let mut inputs: Vec<Input> = Vec::new();
+    inputs.push(input);
+    let repeated_field = RepeatedField::from(inputs);
+    repeated_field
+}
+
+fn read_file(file_name: &String) -> Vec<u8> {
+    let mut file = File::open(file_name).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    buffer
+}
+
+fn call_opt() -> CallOption {
     let api_key = env::var("CLARIFAI_API_KEY")
         .expect("Please set an environmental variable CLARIFAI_API_KEY");
     let auth = "Key ".to_owned() + &api_key;
-    let general_model_id = "aaa03c23b3724a16a56b629203edc62c";
-
-    let ch = insecure_grpc();
-    let client = V2Client::new(ch);
 
     let mut builder = MetadataBuilder::with_capacity(3);
     builder
@@ -40,23 +77,10 @@ fn main() {
     ;
     let metadata = builder.build();
     let call_opt = CallOption::default().headers(metadata);
+    call_opt
+}
 
-    let mut req = PostModelOutputsRequest::default();
-    req.set_model_id(general_model_id.parse().unwrap());
-
-    let mut image = Image::new();
-    image.set_base64(read_file(file_name));
-    let mut data = Data::new();
-    data.set_image(image);
-    let mut input = Input::new();
-    input.set_data(data);
-    let mut inputs: Vec<Input> = Vec::new();
-
-    inputs.push(input);
-    req.set_inputs(RepeatedField::from(inputs));
-    let response = client.post_model_outputs_opt(&req, call_opt).expect("rpc");
-
-    let status = response.get_status();
+fn exit_on_failure(status: &Status) {
     if status.get_code() != StatusCode::SUCCESS {
         println!("Failure response:");
         println!("\t{}", status.get_code().value());
@@ -64,16 +88,4 @@ fn main() {
         println!("\t{}", status.get_details());
         exit(1);
     }
-
-    println!("Predicted concepts:");
-    for concept in response.get_outputs()[0].get_data().get_concepts() {
-        println!("\t{}: {}", concept.get_name(), concept.get_value());
-    }
-}
-
-fn read_file(file_name: &String) -> Vec<u8> {
-    let mut file = File::open(file_name).unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-    buffer
 }
