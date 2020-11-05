2a7d13785dffbf7d75cb30419e6d03f686f2804d
Rok Povsic <rok.povsic@clarifai.com>
2020-11-05 21:13:16 +0100

    Remove the insecure_grpc channel

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,4 @@ pub mod clarifai_channel {
         return ChannelBuilder::new(env)
             .secure_connect("api.clarifai.com", ChannelCredentialsBuilder::new().build());
     }
-
-    pub fn insecure_grpc() -> Channel {
-        let env = Arc::new(EnvBuilder::new().build());
-        return ChannelBuilder::new(env).connect("api-grpc.clarifai.com:18080");
-    }
 }
