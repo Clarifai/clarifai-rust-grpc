bd8290c2b5629c00dc5f8e11b96076ab730ccfbf
Rok Povsic <rok.povsic@clarifai.com>
2020-11-05 20:01:27 +0100

    Add more tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,42 +1,234 @@
 use std::env;
 
 use grpcio::{CallOption, MetadataBuilder};
-use protobuf::{ProtobufEnum, RepeatedField};
+use protobuf::{ProtobufEnum, RepeatedField, SingularPtrField};
 
 extern crate clarifai_grpc;
 
 use clarifai_grpc::clarifai_channel;
-use clarifai_grpc::grpc::resources::{Data, Image, Input};
-use clarifai_grpc::grpc::service::PostModelOutputsRequest;
+use clarifai_grpc::grpc::resources::{Data, Image, Input, Concept};
+use clarifai_grpc::grpc::service::{GetModelRequest, ListModelsRequest, PostModelOutputsRequest, PostInputsRequest, GetInputRequest, PatchInputsRequest, DeleteInputRequest};
 use clarifai_grpc::grpc::service_grpc::V2Client;
 use clarifai_grpc::grpc::status::Status;
 use clarifai_grpc::grpc::status_code::StatusCode;
+use std::thread::sleep;
+use std::time::Duration;
 
 const GENERAL_MODEL_ID: &str = "aaa03c23b3724a16a56b629203edc62c";
+const TRUCK_IMAGE_URL: &str = "https://samples.clarifai.com/red-truck.png";
+
+#[test]
+fn test_get_model() {
+    let request = GetModelRequest {
+        model_id: GENERAL_MODEL_ID.to_string(),
+        ..Default::default()
+    };
+
+    let response = client()
+        .get_model_opt(&request, call_opt())
+        .expect("Failure");
+
+    assert_success_response(response.get_status());
+
+    assert_eq!("general", response.get_model().name);
+}
+
+#[test]
+fn test_list_model_with_pagination() {
+    let request = ListModelsRequest {
+        per_page: 2,
+        ..Default::default()
+    };
+
+    let response = client()
+        .list_models_opt(&request, call_opt())
+        .expect("Failure");
+
+    assert_success_response(response.get_status());
+
+    assert_eq!(2, response.get_models().len());
+}
 
 #[test]
 fn test_post_model_outputs_url() {
-    let mut req = PostModelOutputsRequest::default();
-    req.set_model_id(GENERAL_MODEL_ID.parse().unwrap());
-
-    let mut image = Image::new();
-    image.set_url("https://samples.clarifai.com/dog2.jpeg".parse().unwrap());
-    let mut data = Data::new();
-    data.set_image(image);
-    let mut input = Input::new();
-    input.set_data(data);
-    let mut inputs: Vec<Input> = Vec::new();
-    inputs.push(input);
-    req.set_inputs(RepeatedField::from(inputs));
+    let request = PostModelOutputsRequest {
+        model_id: GENERAL_MODEL_ID.to_string(),
+        inputs: RepeatedField::from(vec![Input {
+            data: SingularPtrField::some(Data {
+                image: SingularPtrField::some(Image {
+                    url: "https://samples.clarifai.com/dog2.jpeg".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }]),
+        ..Default::default()
+    };
 
     let response = client()
-        .post_model_outputs_opt(&req, call_opt())
+        .post_model_outputs_opt(&request, call_opt())
         .expect("Failure");
 
-    let status = response.get_status();
-    assert_success_response(status);
+    assert_success_response(response.get_status());
 
     assert_ne!(0, response.get_outputs()[0].get_data().get_concepts().len());
+}
+
+#[test]
+fn test_failed_post_model_outputs() {
+    let request = PostModelOutputsRequest {
+        model_id: GENERAL_MODEL_ID.to_string(),
+        inputs: RepeatedField::from(vec![Input {
+            data: SingularPtrField::some(Data {
+                image: SingularPtrField::some(Image {
+                    url: "https://samples.clarifai.com/non-existing-image.jpeg".to_string(),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }]),
+        ..Default::default()
+    };
+
+    let response = client()
+        .post_model_outputs_opt(&request, call_opt())
+        .expect("Failure");
+
+    assert_eq!(StatusCode::FAILURE, response.get_status().code);
+}
+
+#[test]
+fn test_mixed_success_post_model_outputs() {
+    let request = PostModelOutputsRequest {
+        model_id: GENERAL_MODEL_ID.to_string(),
+        inputs: RepeatedField::from(vec![
+            Input {
+                data: SingularPtrField::some(Data {
+                    image: SingularPtrField::some(Image {
+                        url: "https://samples.clarifai.com/dog2.jpeg".to_string(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+            Input {
+                data: SingularPtrField::some(Data {
+                    image: SingularPtrField::some(Image {
+                        url: "https://samples.clarifai.com/non-existing-image.jpeg".to_string(),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            },
+        ]),
+        ..Default::default()
+    };
+
+    let response = client()
+        .post_model_outputs_opt(&request, call_opt())
+        .expect("Failure");
+
+    assert_eq!(StatusCode::MIXED_STATUS, response.get_status().code);
+    assert_eq!(StatusCode::SUCCESS, response.get_outputs()[0].get_status().code);
+    assert_eq!(StatusCode::INPUT_DOWNLOAD_FAILED, response.get_outputs()[1].get_status().code);
+}
+
+#[test]
+fn test_post_patch_and_delete_input() {
+    let post_inputs_response = client()
+        .post_inputs_opt(
+            &PostInputsRequest {
+                inputs: RepeatedField::from(vec![
+                    Input {
+                        data: SingularPtrField::some(Data {
+                            image: SingularPtrField::some(Image {
+                                url: TRUCK_IMAGE_URL.to_string(),
+                                allow_duplicate_url: true,
+                                ..Default::default()
+                            }),
+                            concepts: RepeatedField::from(vec![
+                                Concept {
+                                    id: "red-truck".to_string(),
+                                    ..Default::default()
+                                }
+                            ]),
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    }
+                ]),
+                .. Default::default()
+            },
+            call_opt()
+        )
+        .expect("Failure");
+
+    assert_success_response(post_inputs_response.get_status());
+
+    let input_id = post_inputs_response.get_inputs().get(0).expect("Failure").get_id();
+    loop {
+        let get_input_response = client()
+            .get_input_opt(
+                &GetInputRequest {
+                    input_id: input_id.to_string(),
+                    ..Default::default()
+                },
+                call_opt()
+            )
+            .expect("Failure");
+        assert_success_response(get_input_response.get_status());
+        let input_status_code = get_input_response.get_input().get_status().get_code();
+        if input_status_code == StatusCode::INPUT_DOWNLOAD_SUCCESS {
+            break;
+        }
+        if input_status_code != StatusCode::INPUT_DOWNLOAD_PENDING &&
+            input_status_code != StatusCode::INPUT_DOWNLOAD_IN_PROGRESS {
+            panic!(format!("Waiting for input ID {} failed, status code is {:?}", input_id, input_status_code));
+        }
+        sleep(Duration::from_secs(1));
+    }
+
+    let patch_inputs_response = client()
+        .patch_inputs_opt(
+            &PatchInputsRequest {
+                action: "overwrite".to_string(),
+                inputs: RepeatedField::from(vec![
+                    Input {
+                        id: input_id.to_string(),
+                        data: SingularPtrField::some(Data {
+                            concepts: RepeatedField::from(vec![
+                                Concept {
+                                    id: "very-red-truck".to_string(),
+                                    ..Default::default()
+                                }
+                            ]),
+                            ..Default::default()
+                        }),
+                        ..Default::default()
+                    }
+                ]),
+                ..Default::default()
+            },
+            call_opt()
+        )
+        .expect("Failure");
+
+    assert_success_response(patch_inputs_response.get_status());
+
+    let delete_input_response = client()
+        .delete_input_opt(
+            &DeleteInputRequest {
+                input_id: input_id.to_string(),
+                ..Default::default()
+            },
+            call_opt()
+        )
+        .expect("Failure");
+    assert_success_response(delete_input_response.get_status());
 }
 
 fn client() -> V2Client {
@@ -47,7 +239,7 @@ fn client() -> V2Client {
 fn call_opt() -> CallOption {
     let api_key = env::var("CLARIFAI_API_KEY")
         .expect("Please set an environmental variable CLARIFAI_API_KEY");
-    let auth = "Key ".to_owned() + &api_key;
+    let auth = "Key ".to_string() + &api_key;
 
     let mut builder = MetadataBuilder::with_capacity(1);
     builder.add_str("Authorization", &auth).unwrap();
