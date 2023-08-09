e597be553933f647d32119a3bf6ca20373b80288
Yvette Zhang <yvette@clarifai.com>
2023-08-09 17:30:26 -0400

    update tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,13 @@ pub mod grpc {
     pub mod service_grpc;
     pub mod status;
     pub mod status_code;
+    mod annotations;
+    mod matrix;
+    mod extensions;
+    mod extension;
+    mod scope;
+    mod http;
+    mod types;
 }
 
 pub mod clarifai_channel {
@@ -16,10 +23,12 @@ pub mod clarifai_channel {
 
         let grpc_base_url =
             env::var("CLARIFAI_GRPC_BASE").unwrap_or("api.clarifai.com".to_string());
+        
+        let mut channel_builder = ChannelBuilder::new(environment);
+        channel_builder = channel_builder.set_credentials(ChannelCredentialsBuilder::new().build());
 
-        return ChannelBuilder::new(environment).secure_connect(
-            grpc_base_url.as_str(),
-            ChannelCredentialsBuilder::new().build(),
+        return channel_builder.connect(
+            grpc_base_url.as_str()
         );
     }
 }
